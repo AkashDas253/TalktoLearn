30e8a107c8d1620b6d51eac99743d6ca918fcc40
greedyknapsack <subhamdas541.sd@gmail.com>
2024-03-17 02:33:47 +0530

    Readme

--- js/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    translate_key:'d86909b32fe14748a021eaa21284a90d',
-    bot_name : 'ChatandPractice-bot',
-    bot_key:'869f5be5ecb441e8b65543d490ff0f49',
-    KEY_2 : '101010'
-  }
-
-  export default config;
